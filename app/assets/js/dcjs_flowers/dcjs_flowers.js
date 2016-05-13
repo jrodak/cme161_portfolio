dbb4d5b7b58a0d8eb0a6010b9e6117d6000c4b99
Jordan Rodak <jrodak@stanford.edu>
2016-05-13 13:35:18 -0700

    updated flower viz

--- app/assets/js/dcjs_flowers/dcjs_flowers.js
+++ app/assets/js/dcjs_flowers/dcjs_flowers.js
@@ -13,21 +13,21 @@ data is in this format
 
 // on load data {
 
-	// crossfilter
+  // crossfilter
   
   // dimensions for sepal_length, sepal_width, petal_length, petal_width, species
   
   // unique values for species (hint: underscore.js)
 
-	// bar charts for sepal_length, sepal_width, petal_length, petal_width, species 
+  // bar charts for sepal_length, sepal_width, petal_length, petal_width, species 
 
-	// render
+  // render
   
 // } end load data
 
 
 d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function(remote_json){
-	
+  
   window.remote_json = remote_json;
   
   // crossfilter
@@ -53,7 +53,26 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
   var petal_length_sum = petal_length.group().reduceSum(function(d) { return d.petal_length; });
   var petal_width_sum  = petal_width.group().reduceSum(function(d) { return d.petal_width; });
   
+  var reduce_init = function() {
+    return {
+      "count" : 0,
+      "total": 0
+    };
+  };
   
+  var reduce_add = function(p, v, nf) {
+    ++p.count;
+    p.total += v.sepal_length;
+    return p;
+  };
+  
+  var reduce_remove = function(p, v, nf) {
+    --p.count;
+    p.total -= v.sepal_length;
+    return p;
+  };
+  
+  var species_sepal_length_sum = species.group().reduce(reduce_add, reduce_remove, reduce_init);
  
   // implement unique species name extraction
 
@@ -82,7 +101,7 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
   
   
    var sepal_width_chart = dc
-   		.barChart("#sepal_width_chart")
+      .barChart("#sepal_width_chart")
       .width(250)
       .height(200)
       .dimension(sepal_width)
@@ -92,7 +111,7 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
       .xUnits(dc.units.fp.precision(0.5));
       
     var petal_length_chart = dc
-   		.barChart("#petal_length_chart")
+      .barChart("#petal_length_chart")
       .width(250)
       .height(200)
       .dimension(petal_length)
@@ -102,7 +121,7 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
       .xUnits(dc.units.fp.precision(0.5));
       
     var petal_width_chart = dc
-   		.barChart("#petal_width_chart")
+      .barChart("#petal_width_chart")
       .width(250)
       .height(200)
       .dimension(petal_width)
@@ -111,11 +130,34 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
       .x( d3.scale.linear().domain([1,5]) )
       .xUnits(dc.units.fp.precision(0.5));
       
+    // add table of average sepal length
+    // Learned how from http://dc-js.github.io/dc.js/examples/table-on-aggregated-data.html     
+    rank = function (p) { return '';};
+    var table = dc
+      .dataTable("#averages_table")
+      .width(250)
+      .height(200)
+      .dimension(species_sepal_length_sum)
+      .group(rank)
+      .order(d3.descending)
+      .columns([
+        function(d) {
+          return d.key;
+        },
+        function (d) { 
+          if (d.value.count)
+            // round to nearest 1/16 inch
+            return Math.round(d.value.total / d.value.count * 16) / 16;
+          else 
+            return 0;
+        }]);
+      
+      
     
       
-  	var showButton = function() {
-    	if (species_chart.filters().length > 0) {
-      	d3.select(".btn-btn")
+    var showButton = function() {
+      if (species_chart.filters().length > 0) {
+        d3.select(".btn-btn")
           .remove();
         
         d3.select("#resetButton")
@@ -129,10 +171,10 @@ d3.json("http://tranquil-peak-82564.herokuapp.com/api/v1.0/data/iris/", function
               species_chart.filter(null);
               dc.redrawAll();
           });
-          	
+            
       } else {
-      		 d3.select(".btn-btn")
-      				.remove();
+           d3.select(".btn-btn")
+              .remove();
       }
     };
     
